5e89594994789e7c9cabe1a49409f69f7cebf6dc
mamtakurdia808-code <mamtakurdia808@gmail.com>
2026-04-06 17:15:24 +0530

    Admin dashboard

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ import registrationRoutes from "./routes/registration.routes";
 import adminRoutes from "./routes/admin.routes";
 import path from "path";
 import telegramRoutes from "./routes/telegram.routes";
-import { initTelegram } from "./telegramClient";
+//import { initTelegram } from "./telegramClient";
 
 const app = express();
 
@@ -60,7 +60,7 @@ app.use("/api/telegram", telegramRoutes);
 const PORT = process.env.PORT || 5000;
 
 async function startServer() {
-  await initTelegram();
+  //await initTelegram();
 
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
